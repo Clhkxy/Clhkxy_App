c0aa7f38c5fdfbe4dd0fad86e8aab30139ae673b
Clhkxy <125267791+Clhkxy@users.noreply.github.com>
2025-04-13 00:22:50 +0800

    Update Clhkxy_App.py
    增加路由@Clhkxy_App.route("/short", methods=["GET", "POST"])

--- Clhkxy_App.py
+++ Clhkxy_App.py
@@ -214,6 +214,7 @@ def change_password():
 
 # 原 Short.py 中的路由：主页，显示输入框
 @Clhkxy_App.route("/short", methods=["GET", "POST"])
+@Clhkxy_App.route("/short/", methods=["GET", "POST"])
 def short_index():
     if request.method == "POST":
         long_url = request.form["long_url"]
